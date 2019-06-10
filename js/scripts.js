11aec1370dd9b1756a81f12ecda24437e3e0a86c
jenny <jenniffer>
2019-06-10 13:06:25 +0300

    modified my css and html and scripts

--- js/scripts.js
+++ js/scripts.js
@@ -17,37 +17,38 @@ $("#management").click(function(){
 })
 
 /******************* PORTFOLIO *************************************/
-// $(document).ready(function() {
-//   $('.text').hide();
-//   $('img').animate({
-//       opacity:1
+$(document).ready(function() {
+  $('.text').hide();
+  $('img').animate({
+      opacity:1
       
-//   });
+  });
 
-//   $('img').hover(function() {
-//       $(this).stop().animate({opacity:4},200);
-//       $('.text').fadeIn();
+  $('img').hover(function() {
+      $(this).stop().animate({opacity:4},200);
+      $('.text').fadeIn();
 
-//   }, function() {
-//       $(this).stop().animate({opacity:1},500)
-//       $('.text').fadeOut();
-//   });
+  }, function() {
+      $(this).stop().animate({opacity:1},500)
+      $('.text').fadeOut();
+  });
 
 /*************** FORM SUBMISSION *********************************/
-$("#submission").submit(function(event){
-  var data_1 = $("input:first").val();
-  var data_2 = $("input#mail").val();
-  if(data_1 && data_2 != ""  ){
-    $(".message").text("Thank you " + data_1 + " Your message has been received");
-    $(".message").removeClass("error");
-    $(".message").addClass("success");
-  }else{
-    $(".message").text("Error!!! Please fill in all the details correctly");
-    $(".message").addClass("error");
-  }
-  event.preventDefault();
-});
+$(document).ready(function() {
+  $("form#contacts").submit(function(event) {
 
+    event.preventDefault();
+    var name = $.trim($('#name').val());
+    var email = $.trim($('#email').val());
+    if ((name === '') || (email === '')) {
+      alert('Please enter a name and your email address to continue.');
+      return false;
+    } else {
+      alert("Hello " + name + "Your message has been received, thankyou.");
+      $(".submit")[0].reset();
+    }
+  });
+});
 
 /************** PORTFOLIO ********************************/
 // $(document).ready(function(){
